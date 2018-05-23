74140d6a502459dda3e6bea5d838586d8849cc4a
MoZhuXuan10 <1214766435@qq.com>
2018-05-23 14:22:50 +0800

    MyBatis二级缓存

---
+++ 11MyBatis/src/main/java/com/syl/dao/TeacherDao.java
@@ -0,0 +1,18 @@
+package com.syl.dao;
+
+import com.syl.bean.Teacher;
+
+import java.io.Serializable;
+
+public interface TeacherDao {
+    /**
+     * 根据id查询指定的老师信息
+     */
+    Teacher selectTeacherByID(Serializable id);
+
+    /**
+     * 新增teacher
+     */
+    void   insertTeacher(Teacher teacher);
+
+}
