cf1aa2a73d59fb473a4fb6830f4c3ec2209e773c
MoZhuXuan10 <1214766435@qq.com>
2018-05-23 08:33:30 +0800

    关联关系多对多

---
+++ 09BaseMyBatis/src/main/java/com/syl/dao/TeacherDao.java
@@ -0,0 +1,16 @@
+package com.syl.dao;
+
+import com.syl.bean.Student;
+import com.syl.bean.Teacher;
+
+import java.io.Serializable;
+public interface TeacherDao {
+    /**
+     * 根据学生的编号 查询出对应的所有老师信息
+     */
+    Student  selectTeachersBySid(Serializable id);
+    /**
+     * 根据老师的编号 查询出对应的所有学生信息
+     */
+    Teacher selectStudentsByTid(Serializable id);
+}
